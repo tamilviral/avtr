e6a7a3fcc324beb0c4b3257ff1519806ddc72ed5
PDF Conver Tool <125113235+tamilviral@users.noreply.github.com>
2026-05-19 13:14:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2147,6 +2147,13 @@
                 updateTransactionsUI();
             }
         });
+
+        // Register PWA Service Worker for offline capability
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./sw.js')
+                .then((reg) => console.log('[PWA] Service Worker registered successfully:', reg.scope))
+                .catch((err) => console.error('[PWA] Service Worker registration failed:', err));
+        }
     }
 
     // Start!
