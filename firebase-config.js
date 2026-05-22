216563c70fa67063a2b733695a7d58aa1d75592c
PDF Conver Tool <125113235+tamilviral@users.noreply.github.com>
2026-05-22 11:21:35 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyAamiz5i_njeSZ9LaAXedxtMMlDVm2Mw0w",
-    authDomain: "avtr-7a851.firebaseapp.com",
-    projectId: "avtr-7a851",
-    storageBucket: "avtr-7a851.firebasestorage.app",
-    messagingSenderId: "216465267194",
-    appId: "1:216465267194:web:15b44acd48242b71bb6c43"
-};
+  authDomain: "avtr-7a851.firebaseapp.com",
+  databaseURL: "https://avtr-7a851-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "avtr-7a851",
+  storageBucket: "avtr-7a851.firebasestorage.app",
+  messagingSenderId: "216465267194",
+  appId: "1:216465267194:web:15b44acd48242b71bb6c43"
 
 // Initialize Firebase
 if (!firebase.apps.length) {
